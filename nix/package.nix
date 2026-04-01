42b3df8b3712cc55103f10b840a9719c5e0a7b9e
Giovanni d'Amelio <gio@damelio.net>
2026-04-01 01:16:19 -0700

    Add nix package for surrealist web build

---
+++ nix/package.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  stdenv,
+  bun2nix,
+  ...
+}:
+stdenv.mkDerivation {
+  pname = "surrealist-web";
+  version = "3.7.3";
+
+  src = lib.cleanSourceWith {
+    src = ./..;
+    filter = path: type: let
+      baseName = baseNameOf path;
+    in
+      !(builtins.elem baseName [
+        "node_modules"
+        "dist"
+        ".devenv"
+        ".direnv"
+        "target"
+        "src-tauri"
+        ".git"
+        "result"
+        ".claude"
+      ]);
+  };
+
+  nativeBuildInputs = [
+    bun2nix.hook
+  ];
+
+  bunDeps = bun2nix.fetchBunDeps {
+    bunNix = ../bun.nix;
+  };
+
+  dontUseBunBuild = true;
+  dontUseBunInstall = true;
+
+  buildPhase = ''
+    export HOME=$TMPDIR
+    export VITE_SURREALIST_DOCKER=true
+    bun run build
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r dist/* $out/
+  '';
+
+  meta = with lib; {
+    description = "Surrealist - SurrealDB web interface";
+    homepage = "https://github.com/surrealdb/surrealist";
+  };
+}
